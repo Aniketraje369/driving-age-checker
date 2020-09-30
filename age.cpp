81af51810d0e98bb83abdb8bf5b19e79822a83cd
aniketraje2004 <67407093+aniketraje2004@users.noreply.github.com>
2020-10-01 00:24:00 +0530

    Create age.cpp

---
+++ age.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+cout<<"Enter your Age";
+int age;
+cin>>age;
+
+if(age<=18){
+ cout<<"you can drive the vehicle";
+ }
+ 
+ else{
+ cout<<"you cannot drive the vehicle";
+ }
+
+
+return;
+}
